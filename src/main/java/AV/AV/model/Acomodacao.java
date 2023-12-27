ae318c63f8a38dda04d747951f2b7e68d964b958
Andriéria Dantas <andrieria.ad@gmail.com>
2023-12-27 08:03:34 -0300

    Adicionando atributos da entidade Acomodacao

--- src/main/java/AV/AV/model/Acomodacao.java
+++ src/main/java/AV/AV/model/Acomodacao.java
@@ -1,7 +1,35 @@
 package AV.AV.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="acomodacoes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Acomodacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String nome;
+    private String localizacao;
+    private int numero_registro;
+    private int quantidade_quartos;
+    
+    /*falta anfitriao */
 
 
+
+
+    /*ID, nome, localização, número de registro, quantidade de quartos
+disponíveis, anfitrião.*/
+}
